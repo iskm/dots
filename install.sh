3a6d0b415555c4f28afb250b9b7cf9cc27b0dbc9
Ibrahim Mkusa <ibrahimmkusa@gmail.com>
2024-08-08 21:38:48 -0400

    cleaned up install.sh

--- install.sh
+++ install.sh
@@ -11,19 +11,21 @@ if [[ -f /etc/os-release ]]; then
 
   case "$ID_LIKE" in
     debian)
-      echo "Running on debian-family. Installing core packages"
+      echo "Running on debian-family.."
       package_manager=apt
-      sudo $package_manager install -y vim-nox git stow curl ranger tmux
+      vim="vim-nox"
       ;;
     fedora)
-      echo "Running on debian-family. Installing core packages"
+      echo "Running on debian-family.."
       package_manager=fedora
-      sudo $package_manager install -y vim-enhanced git stow curl ranger tmux
+      vim="vim-enhanced"
   esac
 else
   echo "You are running an unrecognized family of os. Quitting..."
   exit 1
 fi
 
+echo "Installing packages"
+sudo $package_manager install -y $vim git stow curl ranger tmux
 # use gnu stow to symlink config files to home directory
 stow bash git ranger shellenv tmux vim
